9582bda6dfc022757a3923788e44f40d38a819da
cop4814 <32044209+cop4814@users.noreply.github.com>
2017-09-17 18:01:51 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_Solution
+{
+    public class Game
+    {
+        public string Team1 { get; set; }
+        public int Team1score { get; set; }
+        public string Team2 { get; set; }
+        public int Team2score { get; set; }
+
+        public Game() {  }
+
+        public Game(string Team1, int Team1score, 
+            string Team2, int Team2score)
+        {
+            this.Team1 = Team1;
+            this.Team1score = Team1score;
+            this.Team2 = Team2;
+            this.Team2score = Team2score;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("{0) ({1} -- {2} ({3})", 
+                Team1, Team1score, Team2, Team2score);
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace Lab_Solution
+{
+    class GameFactory
+    {
+        List<Game> gameList = new List<Game>();
+        public string FilePath { get; set; }
+
+        public void CreateGameList()
+        {
+            Game g = null;
+            g = new Game("Hornets", 50, "Bulldogs", 75);
+            gameList.Add(g);
+            g = new Game("Wasps", 52, "Panthers", 65);
+            gameList.Add(g);
+            g = new Game("Bulls", 54, "Bears", 75);
+            gameList.Add(g);
+            g = new Game("Ducks", 66, "Swans", 65);
+            gameList.Add(g);
+            g = new Game("Hornets", 82, "Panthers", 75);
+            gameList.Add(g);
+            g = new Game("Weasels", 80, "Warthogs", 65);
+            gameList.Add(g);
+
+
+        }
+
+        public Boolean SerializeGameList()
+        {
+            try
+            {
+                XmlSerializer serial = new XmlSerializer(gameList.GetType());
+                StreamWriter sw = new StreamWriter(FilePath);
+                serial.Serialize(sw, gameList);
+                sw.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
